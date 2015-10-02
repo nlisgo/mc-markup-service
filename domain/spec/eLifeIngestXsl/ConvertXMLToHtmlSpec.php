e97ab10bbb525a05d8a5de2375f3feab6dc51923
nlisgo <nathan@aeopus.com>
2015-10-02 20:28:04 +0100

    Add test coverage

---
+++ domain/spec/eLifeIngestXsl/ConvertXMLToHtmlSpec.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace spec\eLifeIngestXsl;
+
+use DOMDocument;
+use eLifeIngestXsl\ConvertXML\XMLString;
+use PhpSpec\Exception\Example\FailureException;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
+
+class ConvertXMLToHtmlSpec extends ObjectBehavior {
+  private function setupXML($filename = '00288-vor') {
+    $xml = XMLString::fromString(file_get_contents(dirname(__FILE__) . '/../../../tests/fixtures/jats/' . $filename . '.xml'));
+    $this->beConstructedWith($xml);
+  }
+
+  public function it_might_have_an_abstract() {
+    $this->setupXML();
+    $abstract = file_get_contents(dirname(__FILE__) . '/../../../tests/fixtures/html/00288-vor-section-abstract.html');
+    $this->getAbstract()->shouldHaveHTML($abstract);
+  }
+
+  public function it_might_have_a_digest() {
+    $this->setupXML();
+    $digest = file_get_contents(dirname(__FILE__) . '/../../../tests/fixtures/html/00288-vor-section-digest.html');
+    $this->getDigest()->shouldHaveHTML($digest);
+  }
+
+  public function it_might_not_have_a_digest() {
+    $this->setupXML('01911-poa');
+    $this->getDigest()->shouldBeNull();
+  }
+
+  public function it_might_have_a_references_section() {
+    $this->setupXML();
+    $this->getReferences()->shouldNotBeNull();
+  }
+
+  public function it_might_not_have_a_references_section() {
+    $this->setupXML('01911-poa');
+    $this->getReferences()->shouldBeNull();
+  }
+
+  public  function it_might_have_an_acknowledgements_section() {
+    $this->setupXML();
+    $this->getAcknowledgements()->shouldNotBeNull();
+  }
+
+  public  function it_might_not_have_an_acknowledgements_section() {
+    $this->setupXML('01911-poa');
+    $this->getAcknowledgements()->shouldBeNull();
+  }
+
+  public  function it_might_have_a_decision_letter() {
+    $this->setupXML();
+    $this->getDecisionLetter()->shouldNotBeNull();
+  }
+
+  public  function it_might_not_have_a_decision_letter() {
+    $this->setupXML('01911-poa');
+    $this->getDecisionLetter()->shouldBeNull();
+  }
+
+  public  function it_might_have_a_author_response() {
+    $this->setupXML();
+    $this->getAuthorResponse()->shouldNotBeNull();
+  }
+
+  public  function it_might_not_have_a_author_response() {
+    $this->setupXML('01911-poa');
+    $this->getAuthorResponse()->shouldBeNull();
+  }
+
+  public function getMatchers() {
+    return [
+      'haveHTML' => function ($actual, $expected) {
+        $html_prefix = '<meta http-equiv="content-type" content="text/html; charset=utf-8">';
+        $wrapper = 'wrapper';
+        libxml_use_internal_errors(TRUE);
+        $actualDom = new DOMDocument();
+        $actualDom->loadHTML($html_prefix . '<' . $wrapper . '>' . $actual . '</' . $wrapper . '>');
+
+        $expectedDom = new DOMDocument();
+        $expectedDom->loadHTML($html_prefix . '<' . $wrapper . '>' . $expected . '</' . $wrapper . '>');
+
+        // Remove white space between tags.
+        $from = ['/\>[^\S ]+/s', '/[^\S ]+\</s', '/(\s)+/s', '/> </s'];
+        $to = ['>', '<', '\\1', '><'];
+        $clean_actual = preg_replace($from, $to, $this->getInnerHtml($actualDom->getElementsByTagName($wrapper)->item(0)));
+        $clean_expected = preg_replace($from, $to, $this->getInnerHtml($expectedDom->getElementsByTagName($wrapper)->item(0)));
+
+        if ($clean_actual != $clean_expected) {
+          throw new FailureException(sprintf('Expected "%s" but found "%s".', $clean_expected, $clean_actual));
+        }
+        return TRUE;
+      },
+    ];
+  }
+
+  /**
+   * Get inner HTML.
+   */
+  function getInnerHtml($node) {
+    $innerHTML = '';
+    $children = $node->childNodes;
+    foreach ($children as $child) {
+      $innerHTML .= $child->ownerDocument->saveXML($child);
+    }
+
+    return trim($innerHTML);
+  }
+}
---
+++ tests/simpleTest.php
@@ -0,0 +1,151 @@
+<?php
+
+
+use eLifeIngestXsl\ConvertXML\XMLString;
+use eLifeIngestXsl\ConvertXMLToHtml;
+
+class simpleTest extends PHPUnit_Framework_TestCase
+{
+    private $jats_folder = '';
+    private $html_folder = '';
+
+    public function setUp()
+    {
+        $realpath = realpath(dirname(__FILE__));
+        $this->jats_folder = $realpath . '/fixtures/jats/';
+        $this->html_folder = $realpath . '/fixtures/html/';
+    }
+
+    public function testJatsToHtmlAbstract() {
+        $compares = $this->compareHtmlSection('-section-abstract', 'getAbstract');
+        $this->runHtmlComparisons($compares);
+    }
+
+    public function testJatsToHtmlDigest() {
+        $compares = $this->compareHtmlSection('-section-digest', 'getDigest');
+        $this->runHtmlComparisons($compares);
+    }
+
+    public function testJatsToHtmlDecisionLetter() {
+        $compares = $this->compareHtmlSection('-section-decision-letter', 'getDecisionLetter');
+        $this->runHtmlComparisons($compares);
+    }
+
+    public function testJatsToHtmlAuthorResponse() {
+        $compares = $this->compareHtmlSection('-section-author-response', 'getAuthorResponse');
+        $this->runHtmlComparisons($compares);
+    }
+
+    public function testJatsToHtmlAcknowledgements() {
+        $compares = $this->compareHtmlSection('-section-acknowledgements', 'getAcknowledgements');
+        $this->runHtmlComparisons($compares);
+    }
+
+    public function testJatsToHtmlReferences() {
+        $compares = $this->compareHtmlSection('-section-references', 'getReferences');
+        $this->runHtmlComparisons($compares);
+    }
+
+    /**
+     * Compare the expect and actual HTML results.
+     *
+     * @param array[] $compares
+     */
+    protected function runHtmlComparisons($compares) {
+        $this->runComparisons($compares, 'assertEqualHtml');
+    }
+
+    /**
+     * Compare the expect and actual results.
+     *
+     * @param array[] $compares
+     * @param string $method
+     */
+    protected function runComparisons($compares, $method = 'assertEquals') {
+        foreach ($compares as $compare) {
+            call_user_func_array([$this, $method], $compare);
+        }
+    }
+
+    /**
+     * Prepare array of actual and expected results for DOI HTML.
+     */
+    protected function compareDoiHtmlSection($suffix) {
+        $htmls = glob($this->html_folder . '*' . $suffix . '.html');
+        $sections = [];
+
+        foreach ($htmls as $html) {
+            $found = preg_match('/^(?P<filename>[0-9]{5}\-[^\-]+)\-(?P<doi>[^\-]+)' . $suffix . '\.html$/', basename($html), $matches);
+            if ($found) {
+                $sections[] = [
+                    'suffix' => '-' . $matches['doi'] . $suffix,
+                    'doi' => '10.7554/' . $matches['doi'],
+                ];
+            }
+        }
+        $compares = [];
+
+        foreach ($sections as $section) {
+            $compares = array_merge($compares, $this->compareHtmlSection($section['suffix'], 'getDoi', $section['doi']));
+        }
+
+        return $compares;
+    }
+
+    /**
+     * Prepare array of actual and expected results.
+     */
+    protected function compareHtmlSection($suffix, $method, $params = []) {
+        if (is_string($params)) {
+            $params = [$params];
+        }
+        $html_prefix = '<meta http-equiv="content-type" content="text/html; charset=utf-8">';
+        $expected = 'expected';
+        $htmls = glob($this->html_folder . "*" . $suffix . ".html");
+        $compares = [];
+
+        libxml_use_internal_errors(TRUE);
+          foreach ($htmls as $html) {
+            $file = str_replace($suffix, '', basename($html, '.html'));
+            $actual_html = new ConvertXMLToHtml(XMLString::fromString(file_get_contents($this->jats_folder . $file . '.xml')));
+
+            $expectedDom = new DOMDocument();
+            $expected_html = file_get_contents($html);
+            $expectedDom->loadHTML($html_prefix . '<' . $expected . '>' . $expected_html . '</' . $expected . '>');
+
+            $compares[] = [
+                $this->getInnerHtml($expectedDom->getElementsByTagName($expected)->item(0)),
+                call_user_func_array([$actual_html, $method], $params),
+            ];
+        }
+        libxml_clear_errors();
+
+        return $compares;
+    }
+
+    /**
+     * Compare two HTML fragments.
+     */
+    protected function assertEqualHtml($expected, $actual)
+    {
+        $from = ['/\>[^\S ]+/s', '/[^\S ]+\</s', '/(\s)+/s', '/> </s'];
+        $to = ['>', '<', '\\1', '><'];
+        $this->assertEquals(
+            preg_replace($from, $to, $expected),
+            preg_replace($from, $to, $actual)
+        );
+    }
+
+    /**
+     * Get inner HTML.
+     */
+    function getInnerHtml($node) { 
+        $innerHTML= ''; 
+        $children = $node->childNodes; 
+        foreach ($children as $child) { 
+            $innerHTML .= $child->ownerDocument->saveXML($child); 
+        }
+
+        return trim($innerHTML);
+    } 
+}
